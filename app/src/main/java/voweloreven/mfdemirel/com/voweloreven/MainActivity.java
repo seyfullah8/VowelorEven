17d85d78c92d056f1a6acef3e37d0a1e1ab47947
mdemirel <mehmetfurkandemirel@gmail.com>
2015-11-22 15:48:59 -0600

    Graphics Fixed

--- app/src/main/java/voweloreven/mfdemirel/com/voweloreven/MainActivity.java
+++ app/src/main/java/voweloreven/mfdemirel/com/voweloreven/MainActivity.java
@@ -1,12 +1,17 @@
 package voweloreven.mfdemirel.com.voweloreven;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,25 +29,37 @@ public class MainActivity extends AppCompatActivity {
     int puan = 0;
     int count = 45;
     int combo = 0;
+    int id,id2;
     TextView puanGoster;
     TextView timerGoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-
+        final SoundPool soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+        id = soundPool.load(this,R.raw.water,1);
+        id2 = soundPool.load(this,R.raw.wrong,1);
         final TextView ustButton = (TextView) findViewById(R.id.ust);
         final TextView altButton = (TextView) findViewById(R.id.alt);
         final Button yesButton = (Button) findViewById(R.id.yesButton);
         final Button noButton = (Button) findViewById(R.id.noButton);
-        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/goodtimes.ttf");
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/trench.otf");
+        Typeface face1 = Typeface.createFromAsset(getAssets(), "fonts/comic_andy.ttf");
         ustButton.setTypeface(face);
         altButton.setTypeface(face);
+        yesButton.setTypeface(face);
+        noButton.setTypeface(face);
+
         puanGoster = (TextView) findViewById(R.id.puanGoster);
         timerGoster = (TextView) findViewById(R.id.timer);
         puanGoster.setText(String.valueOf(puan));
+        puanGoster.setTypeface(face);
+        timerGoster.setTypeface(face);
         ///////// DEMIREL PUSH YAPTIM
         Thread t = new Thread() {
 
@@ -66,73 +83,98 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
         t.start();
+
+
         editButtons(ustButton, altButton);
         yesButton.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
                 if (!gameOver) {
                     if (ustOpen) {
                         if (vowel) {
                             puan += 10;
                             combo++;
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id, 100, 100, 1, 0, 1);
 
                         } else {
                             puan -= 10;
                             combo = 0;
                             count--;
+                            if(count<0) {count=0; gameOver=true;}
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id2, 100, 100, 1, 0, 1);
                         }
                     }
                     if (altOpen) {
                         if (even) {
                             puan += 10;
                             combo++;
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id, 100, 100, 1, 0, 1);
 
                         } else {
                             puan -= 10;
                             count--;
+                            if(count<0) {count=0; gameOver=true;}
                             combo = 0;
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id2, 100, 100, 1, 0, 1);
                         }
                     }
                     if (combo == 5) {
                         count += 5;
-                        combo=0;
+                        combo = 0;
+                        timerGoster.setText(String.valueOf(count));
                     }
                     puanGoster.setText(String.valueOf(puan));
+
                     editButtons(ustButton, altButton);
                 }
             }
         });
 
         noButton.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
                 if (!gameOver) {
                     if (ustOpen) {
                         if (!vowel) {
                             puan += 10;
                             combo++;
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id, 100, 100, 1, 0, 1);
 
                         } else {
                             puan -= 10;
                             combo = 0;
                             count--;
+                            if(count<0) {count=0; gameOver=true;}
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id2, 100, 100, 1, 0, 1);
                         }
                     }
                     if (altOpen) {
                         if (!even) {
                             puan += 10;
                             combo++;
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id, 100, 100, 1, 0, 1);
 
                         } else {
                             puan -= 10;
                             combo = 0;
                             count--;
+                            if(count<0) {count=0; gameOver=true;}
+                            timerGoster.setText(String.valueOf(count));
+                            soundPool.play(id2, 100, 100, 1, 0, 1);
                         }
                     }
                     if (combo == 5) {
-                        count += 5;
+                        {
+                            count += 5;
+                            combo=0;
+                            timerGoster.setText(String.valueOf(count));
+                        }
                     }
                     puanGoster.setText(String.valueOf(puan));
                     editButtons(ustButton, altButton);
@@ -176,8 +218,6 @@ public class MainActivity extends AppCompatActivity {
         if (randomgame == 0) {
             ustOpen = true;
             altOpen = false;
-
-            Random t = new Random();
             int randomButton = r.nextInt(2);
             if (randomButton == 0) {
                 ustButtonText = RandomLetter() + String.valueOf(RandomNumber());
